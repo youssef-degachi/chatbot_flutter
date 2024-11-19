59d424cfdc68725c73983a775b52e6100f0de529
youssef-degachi <youssefdegachi0@gmail.com>
2024-11-19 15:01:38 +0100

    add chat_provider

---
+++ lib/providers/chat_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../models/chat.dart';
+
+class ChatProvider with ChangeNotifier {
+  List<Chat> _chats = [];
+
+  List<Chat> get chats => _chats;
+
+  void addChat(Chat chat) {
+    _chats.add(chat);
+    notifyListeners();
+  }
+}
